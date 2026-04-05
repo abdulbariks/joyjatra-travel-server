8481f787a6638266e96e6f41a86bf683a0f835ab
abdulbariks <abdulbarik1997m@gmail.com>
2026-04-05 16:43:06 +0600

    reviews.prisma

--- src/app/modules/event/event.controller.ts
+++ src/app/modules/event/event.controller.ts
@@ -1,7 +1,10 @@
 import { Request, Response } from "express";
+import httpStatus from "http-status";
 import { EventService } from "./event.service";
+import { catchAsync } from "../../shared/catchAsync";
+import { sendResponse } from "../../shared/sendResponse";
 
-const CreateEvent = async (req: Request, res: Response) => {
+const CreateEvents = async (req: Request, res: Response) => {
   try {
     // moderatorId usually comes from the decoded token in checkAuth middleware
     const moderatorId = (req as any).user.id; 
@@ -24,6 +27,22 @@ const CreateEvent = async (req: Request, res: Response) => {
     });
   }
 };
+
+const CreateEvent = catchAsync(async (req: Request, res: Response) => {
+    const payload = req.body;
+    const user = req.user;
+    console.log(payload);
+    console.log("user========",user);
+    
+    
+    const result = await EventService.createEvent(user, payload);
+    sendResponse(res, {
+        httpStatusCode: httpStatus.OK,
+        success: true,
+        message: 'Event created successfully',
+        data: result,
+    });
+});
 
 export const EventController = {
   CreateEvent,
--- src/app/modules/event/event.service.ts
+++ src/app/modules/event/event.service.ts
@@ -1,16 +1,26 @@
 
 import { prisma } from "../../../lib/prisma";
+import { IRequestUser } from "../../interfaces/requestUser.interface";
 import { ICreateEventInput } from "./event.interface";
 
 
 
-const createEvent = async (moderatorId: string, payload: ICreateEventInput) => {
+const createEvent = async (user : IRequestUser, payload: ICreateEventInput) => {
+
+  const moderator = await prisma.moderator.findUnique({
+    where: { userId: user.userId } 
+  });
+
+  if (!moderator) {
+    throw new Error("Only users with a Moderator profile can create events.");
+  }
+
   const result = await prisma.event.create({
     data: {
       ...payload,
       startDate: new Date(payload.startDate),
       endDate: new Date(payload.endDate),
-      moderatorId,
+      moderatorId: user.userId,
     },
   });
   return result;
